2672117fa66b5ced6524b117fd998650c8dcb4a0
Anthony Box <ajboxjr@gmail.com>
2017-09-26 01:44:02 -0700

    adds bootstrap and delete

--- app.js
+++ app.js
@@ -17,52 +17,53 @@ var Reviews = mongoose.model('Review',{
     movieTitle: String
 });
 
+app.listen(3000, function () {
+  console.log('Portfolio App listening on port 3000!')
+})
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // override with POST having ?_method=DELETE or ?_method=PUT
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 app.get('/',function(req,res){
     Reviews.find(function(err,reviews){
         res.render('reviews-index', {reviews:reviews})
     })
-})
+});
+
 app.post('/reviews', function (req, res) {
   Reviews.create(req.body, function(err, review) {
     res.redirect('/reviews/' + review._id);
   })
-})
+});
 
 app.get('/reviews/new',function(req,res){
     res.render('reviews-new', {});
-})
+});
 
 app.get('/reviews/:id', function (req, res) {
     Reviews.findById(req.params.id).exec(function(err, review){
              res.render('reviews-show', {review: review});
     })
-})
+});
 app.put('/reviews/:id', function (req, res) {
   Reviews.findByIdAndUpdate(req.params.id,  req.body, function(err, review) {
     res.redirect('/reviews/' + review._id);
   })
-})
+});
 
 app.delete('/reviews/:id', function (req, res) {
-    Review.findByIdAndRemove(req.params.id, function(err,review) {
-        res.redirect('/'+review._id);
+    Reviews.findByIdAndRemove({_id: req.params.id}, function(err) {
+        res.redirect('/');
     })
 })
 app.get('/reviews/:id/edit', function (req, res) {
   Reviews.findById(req.params.id, function(err, review) {
     res.render('reviews-edit', {review: review, ID: req.params.id});
   })
-})
+});
 //app.get('/',function(req,res){
 //    res.render('home', {msg: 'Hello World'});
 //})
-
-app.listen(3000, function () {
-  console.log('Portfolio App listening on port 3000!')
-})
